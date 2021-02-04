09888b88ea7662854e251c14d9f126d3eaa4a58a
Vantol Bennett <teckcare2011@gmail.com>
2021-02-04 05:27:32 +0000

    UserSchema created and methods

--- server/server.js
+++ server/server.js
@@ -1,27 +1,22 @@
 // Both devBundle lines must be commented out during production.
-import devBundle from "./devBundle"
-import config from './../config/config'
-import app from './express'
-import mongoose from 'mongoose'
+import devBundle from './devBundle';
+import config from './../config/config';
+import app from './express';
+import mongoose from 'mongoose';
 
+// import { MongoClient } from 'mongodb'
 
+devBundle.compile(app);
 
-
-import { MongoClient } from 'mongodb'
-
-
-
-devBundle.compile(app)
-
-
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUri, {
-    useNewUrlParser: true,
-    useCreateIndex: true, useUnifiedTopology: true
-})
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
 mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${config.mongoUri}`)
-})
+	throw new Error(`unable to connect to database: ${config.mongoUri}`);
+});
 // const url = process.env.MONGODB_URI || 'mongodb+srv://newUser:Vantol@cluster0.sosjs.mongodb.net/god?retryWrites=true&w=majority'
 // MongoClient.connect(url, (err, db) => {
 //     console.log("Connected successfully to mongodb server")
@@ -36,12 +31,11 @@ mongoose.connection.on('error', () => {
 // app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 //
 
-
 // let port = process.env.PORT || 3000
 //
-app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.info('Server started on port %s.', config.port)
-})
+app.listen(config.port, err => {
+	if (err) {
+		console.log(err);
+	}
+	console.info('Server started on port %s.', config.port);
+});
